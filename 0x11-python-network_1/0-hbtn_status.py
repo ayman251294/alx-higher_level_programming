e9998517bf16fe2e4aadebd781c4c03f625f3d2b
Ayman Nourredine <ayman251294@gmail.com>
2023-10-02 15:39:27 -0700

    0-6files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
+""" A Python script that fetches https://alx-intranet.hbtn.io/status """
 
 
 if __name__ == '__main__':

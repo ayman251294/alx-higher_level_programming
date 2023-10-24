de3bb3fa5fa019b970aff9a9ac4a3fe05ee03706
ayman251294 <ayman251294@gmail.com>
2023-10-24 09:39:23 -0700

    fix task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 // script that reads and prints the content of a file
 
 const file = process.argv[2];
-const fs = require(`fs`);
+const fs = require('fs');
 
 fs.readFile(file, 'utf8', (error, data) => {
 	if (error) {
